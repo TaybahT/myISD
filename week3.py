7e72da3d3317ae0426fdc46ca9ce2df5389ffa9c
TaybahT <33006123+TaybahT@users.noreply.github.com>
2017-10-22 20:19:04 +0100

    transforming string to intergers

---
+++ week3.py
@@ -0,0 +1,15 @@
+num1=input("Enter first number: ")
+num2=input("Enter second muber: ")
+
+sum= float(num1)+float(num2)
+print(sum)
+
+num=4
+num*=2
+num1=num+2
+num1+=3
+print(num1)
+
+print(2/3)
+print(2//3)
+
